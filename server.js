4d9c450b2c74c0f6e64545a6f9cd6ee6e09983c3
CallumRbrts <callum.roberts.pro@gmail.com>
2021-02-26 16:14:52 +0100

    Added data to the click cards, you cna now see the name of the recipes, its ingredients and instructions with an image + Adapted measures to metric standard rather than us standard

--- server.js
+++ server.js
@@ -75,16 +75,31 @@ app.route('/')
 
     //result of get is return through a callback, so I need to do the dynamic html in the callback (variable can't be passed up due to async)
     mongoManager.getFromDB("dailyRecipes", function(result){
-      console.log(result);
+      //console.log(result);
       var recipes = [];
 
 
       for (let i = 0; i < result.length; ++i){
-        var tagline = '<div class="col-md-6 col-lg-4 mb-5"><div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1"><div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100"><div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div></div><img class="img-fluid" src="'+result[i].image+'" alt="" /></div></div>';
-        recipes.push(tagline);
+        //console.log(result[i].extendedIngredients[0]);
+        var ingredients = ""
+        for(let j= 0; j < result[i].extendedIngredients.length; ++j){
+          var metric = result[i].extendedIngredients[j].measures.metric
+          //console.log(metric.amount + " " + metric.unitShort + " " + result[i].extendedIngredients[j].originalName);
+          var ingredient = '<li>'+ Math.round(metric.amount) + " " + metric.unitShort + " " + result[i].extendedIngredients[j].originalName + ' </li>'
+          //ingredients.push(ingredient)
+          ingredients += ingredient
+        }
+        // for (ingredient in result[i].extendedIngredients){
+        //   console.log(ingredient);
+        //   ingredients.push('<li>'+ ingredient.originalString+ ' </li>')
+        // }
+        var tagline = '<div class="col-md-6 col-lg-4 mb-5"><div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal'+i+'"><div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100"><div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div></div><img class="img-fluid" src="'+result[i].image+'" alt="" /></div></div>';
+        var card ='<div class="portfolio-modal modal fade" id="portfolioModal'+i+'" tabindex="-1" role="dialog" aria-labelledby="portfolioModal1Label" aria-hidden="true"><div class="modal-dialog modal-xl" role="document"><div class="modal-content"><button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><i class="fas fa-times"></i></span></button><div class="modal-body text-center"><div class="container"><div class="row justify-content-center"><div class="col-lg-8"><h2 class="portfolio-modal-title text-secondary text-uppercase mb-0" id="portfolioModal1Label">'+result[i].title+'</h2><div class="divider-custom"><div class="divider-custom-line"></div><div class="divider-custom-icon"><i class="fas fa-star"></i></div><div class="divider-custom-line"></div></div><img class="img-fluid rounded mb-5" src="'+result[i].image+'" alt="" /><h2 class="portfolio-modal-title text-secondary text-uppercase mb-0" id="portfolioModal1Label">Ingredients</h2><p class="mb-5">'+ingredients+'</p></br><h2 class="portfolio-modal-title text-secondary text-uppercase mb-0" id="portfolioModal1Label">Instructions</h2></br><p class="mb-5">'+result[i].instructions+'</p><button class="btn btn-primary" data-dismiss="modal"><i class="fas fa-times fa-fw"></i>Add to Cookbook</button></div></div></div></div></div></div></div>'
+
+        recipes.push([tagline, card]);
       }
 
-      console.log(recipes);
+      //console.log(recipes);
 
       res.render(__dirname+'/index.ejs',{
         recipes: recipes
